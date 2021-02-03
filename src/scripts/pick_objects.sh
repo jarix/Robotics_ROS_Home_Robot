831df3d1603f26d17a0561fa023e82cdfedbfc55
Jari Honkanen <Jari@Honkanen.cc>
2021-02-03 02:39:06 +0000

    Made directory paths in bash scripts relative with rospack

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/jaris_house.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/../map/jaris_house.world " &
 sleep 8
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/jaris_house_slam.yaml " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/../map/jaris_house_slam.yaml " &
 sleep 8
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 10
